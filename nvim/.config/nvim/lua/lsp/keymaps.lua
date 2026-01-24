668b038778c68cf5f5c28fc915dfd26d9c7cee91
a-curious-coder <callummclennan1@googlemail.com>
2026-01-24 13:44:42 +0000

    nvim rename keymap change

--- nvim/.config/nvim/lua/lsp/keymaps.lua
+++ nvim/.config/nvim/lua/lsp/keymaps.lua
@@ -14,13 +14,13 @@ M.setup = function(bufnr)
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, vim.tbl_extend("force", opts, { desc = "Go to definition" }))
   vim.keymap.set("n", "gr", vim.lsp.buf.references, vim.tbl_extend("force", opts, { desc = "Show references" }))
   vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, { desc = "Code actions" }))
-  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, vim.tbl_extend("force", opts, { desc = "Rename symbol" }))
+  vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, vim.tbl_extend("force", opts, { desc = "Rename symbol" }))
 
   -- === DIAGNOSTICS ===
   vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, vim.tbl_extend("force", opts, { desc = "Previous diagnostic" }))
   vim.keymap.set("n", "]d", vim.diagnostic.goto_next, vim.tbl_extend("force", opts, { desc = "Next diagnostic" }))
   vim.keymap.set("n", "gl", vim.diagnostic.open_float, vim.tbl_extend("force", opts, { desc = "Show diagnostic" }))
-  
+
   -- Note: Formatting is handled by Conform plugin (<leader>cf in conform.lua)
 end
 
