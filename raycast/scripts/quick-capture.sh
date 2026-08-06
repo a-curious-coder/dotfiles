3c0d5143b2a6c13c35e8055b3683a3034fd6a832
a-curious-coder <45906280+a-curious-coder@users.noreply.github.com>
2026-08-06 19:34:20 +0100

    Add Raycast quick-capture script for Quickdraw (#11)

---
+++ raycast/scripts/quick-capture.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Quick Capture Note
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 📝
+# @raycast.packageName Obsidian
+
+open "obsidian://quick-capture"
